951fce62aa1f46af05f60adcf9482e1150f3efe0
John Blackshear <swjaybee@gmail.com>
2019-05-03 20:49:23 -0500

    init

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
-    var goal, current, redNumber, blueNumber, yellowNumber, greenNumber;
-    var winsCounter = 0;
+    var goal, current, redNumber, blueNumber, yellowNumber, greenNumber, wins;
+    wins = 1;
+    losses = 1;
 
-    var redNumber = Math.floor(Math.random()*12);
-    var yellowNumber = Math.floor(Math.random()*12);
-    var greenNumber = Math.floor(Math.random()*12);
-    var blueNumber = Math.floor(Math.random()*12);
+    var redNumber = Math.floor(Math.random()*12 + 1);
+    var yellowNumber = Math.floor(Math.random()*12 +1);
+    var greenNumber = Math.floor(Math.random()*12 +1);
+    var blueNumber = Math.floor(Math.random()*12 + 1);
 
     $("#red").attr("value", redNumber);
     $("#yellow").attr("value", yellowNumber);
     $("#green").attr("value", greenNumber);
     $("#blue").attr("value", blueNumber);
+    $("winsdiv").attr("value", wins);
+    $("lossdiv").attr("value", losses);
     
     var resetGame = function(){
         goal = Math.floor(Math.random()*101 + 19);
@@ -27,19 +30,24 @@ $(document).ready(function(){
     resetGame();         
 
         $(".button").click(function(){
+            
             current +=parseInt($(this).attr("value"));// makes "current value a number
             console.log(current);
             
             $('#points_total').text(current);
 
+            
+            
             if(current === goal){
-
                 alert("you win");
+                $('#winsdiv').text(wins ++, "wins");
+                resetGame();
             }
             else if(current >= goal){
                 alert("you lose");
+                $('#lossdiv').text(losses ++, "loosses");
+                resetGame();
             }
-
         });
         
     
